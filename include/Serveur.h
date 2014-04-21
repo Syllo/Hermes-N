141907732aaae0aab631cf6ad3cc373d52b8bb48
Maxime Schmitt <maxime.schmitt91@gmail.com>
2014-04-21 18:09:57 +0200

    Add server functions

--- include/Serveur.h
+++ include/Serveur.h
@@ -1,15 +1,27 @@
 #ifndef __SERVEUR_H
 #define __SERVEUR_H
 
+#include <vector>
+#include <string>
+#include <stack>
+
 typedef struct{
-	
-	} server;
-	
-	
+    std::string name;
+    std::string ipaddr;
+} client;
+
+typedef struct{
+    std::string name;
+    std::vector< clients* > clients;
+} alias;
+
 class Serveur{
-	public:
-	
-	private:
+    public:
+        int sendMesg(std::string name);
+        std::stack<std::string>& recvMesg(void);
+    private:
+        std::vector<client> clients;
+        std::vector<alias> aliases;
 }
 
 #endif //__SERVEUR_H
--- src/Serveur.cpp
+++ src/Serveur.cpp
@@ -1,1 +1,6 @@
 #include "Serveur.h"
+
+int Serveur::sendMesg(std::string name){
+}
+std::stack<std::string>& Serveur::recvMesg(void){
+}
